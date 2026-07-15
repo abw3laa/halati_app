ac0aee860ad85e18b98c9a87a927c435a4e873ef
abw3laa <aasra1813@gmail.com>
2026-07-16 01:51:04 +0300

    Fix jcenter in kotlin dsl build gradle

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,8 +1,16 @@
+// تحويل استدعاءات jcenter() القديمة إلى mavenCentral() تلقائياً لمنع فشل بناء المكتبات مثل shared_storage
 allprojects {
     repositories {
         google()
         mavenCentral()
     }
+    
+    // هذا الكود البرمجي يعيد تعريف دالة jcenter لتشير داخلياً إلى mavenCentral
+    val repoHandler = repositories
+    val metaClass = (repoHandler as GroovyObject).metaClass
+    metaClass.setProperty("jcenter", closureOf<Any> {
+        repoHandler.mavenCentral()
+    })
 }
 
 val newBuildDir: Directory =
@@ -15,6 +23,7 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
